5b50b434ba6867d5533fd0022e9296d85f5d359b
nolano78344 <55156573+nolan78344@users.noreply.github.com>
2021-03-08 08:58:09 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -20,26 +20,6 @@ def instructions():
     # Modified:
     return None
 
-def getParams(shape):
-    # Will create a list of questions to be asked depending on the shape.
-    # These will be asked so that the user can enter in appropriate values
-    # input parameter: string 
-    # output parameter: return a list containing the prompts for each shape:
-    # example: ["Enter the radius:","Enter the slant height:","Enter the height:"]
-    prompts
-
-    return prompts
-
-def getInputs(questions):
-    # Will prompt the user for inputs for the shape they.
-    # These will be asked so that the user can enter in appropriate values
-    # It will turn all the input data into a list
-    # input parameter: list containing the prompts/questions
-    # output parameter: return a list containing all the measurements of the shape
-    measurements
-    
-    return measurements
-
 def main():
     # main block of code that will run your program and control program flow
     # You will need to include a while loop to keep repeating the commands until
@@ -71,23 +51,29 @@ main()
 
 
 #me
-def Cube1():
-    a = float(input("What is Edge"))
-    x = Cube2(a,b)
-    print("volume is" + str(x))
+def CubeVolume1():
+    v = float(input("What is Edge"))
+    return v
 
-def Cube2(a):
+def CubeVolume2(a):
     x = (a * a * a)
     return x
 
+def CubeSurfaceArea1():
+    a = float(input("What is Edge"))
+    return a 
 
+def CubeSurfacerea2(a):
+    x = (a * a * 6)
+    return x
 
-
-
-
-def Cone1(r,h):
+def ConeVolume1(r,h):
     #r is raduis
     #h is height
     x = math.pi * (r**2) * (h // 3)
     return x
 
+def ConeVolume2():
+    r = float(input("What is radius"))
+    h = float(input("What is height"))
+    return r,h
