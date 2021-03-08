f8b1315238391ced007f521c3bb92037581a93fa
Liam751 <55156691+Liam751@users.noreply.github.com>
2021-03-08 09:43:47 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -87,3 +87,50 @@ def conicalFrustum(r1,r2,h):
 def inputSphere(r):
     v = math.pi() * 3/4 * r**3
     return v
+
+#cubevolume
+def CubeVolume2(a):
+    x = (a * a * a)
+    return x
+
+#conevolume
+def ConeVolume1(r,h):
+    #r is raduis
+    #h is height
+    x = pi * (r**2) * (h / 3)
+    return x
+
+#pyramidvolume
+def pyramid1(l,w,h):
+    x = (l * w * h /3)
+    return x
+
+#triangularprism
+def triangularprism1(a,b,c,h):
+    x = 0.25 * h * math.sqrt( (-a **4) + 2 * ((a * b)**2) + 2 * ((a * c)**2) - (b**4) + 2 * ((b * c)**2) - (c**4)) 
+    return x
+
+#squareprism
+def squareprism1(a,h):
+    x = ((a**2) * h)
+    return x 
+
+#cylinder
+def Cylinder(r,h):
+    x = pi * (r**2) * h
+    return x
+
+#hemisphere
+def Hemisphere(r):
+    x = (2/3) * pi * (r**3)
+    return x
+
+#ellipsoid
+def Ellipsoid(a,b,c):
+    x = (4/3) * pi * a * b * c
+    return x
+
+#rectangularprism
+def rectangularprism(l,w,h):
+    x = l * w * h
+    return x
